eed88239ed99d9c61649477d2e3a034e80af7e8a
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-05-25 23:40:21 +0600

    reduce function removed

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
 import "./OrderSummary.css";
-import { addFromArray } from '../../utilities/calculatingVal';
 
 const OrderSummary = (props) => {
     const {cart} = props;
